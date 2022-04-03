df6682824c62565fc8e973d8a22ee9c38fde89fa
ApoloGH <apolo-gh@users.noreply.github.com>
2022-04-03 14:18:43 -0400

    test: add unit-tests for examples

---
+++ examples/example1.test.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { jest } from '@jest/globals';
+
+global.console = { log: jest.fn() };
+
+describe('Example 1', () => {
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  test('Example 1', async () =>
+    import('./readme.js').then(() => {
+      expect(console.log).toHaveBeenCalledTimes(2);
+      expect(console.log.mock.calls[0]).toEqual(['[object Model]']);
+      expect(console.log.mock.calls[1]).toEqual([
+        JSON.stringify(
+          {
+            firstName: 'Trygve',
+            lastName: 'Reenskaug',
+            yearBorn: 1930,
+            verified: null,
+          },
+          null,
+          '  '
+        ),
+      ]);
+    }));
+});
--- examples/readme.js
+++ examples/readme.js
@@ -1,29 +1,25 @@
 import Model from '../src/model.js';
 
-const example1 = () => {
-  const Profile = new Model({
-    firstName: String,
-    lastName: String,
-    yearBorn: Number,
-    'verified?': Boolean,
-  });
+const Profile = new Model({
+  firstName: String,
+  lastName: String,
+  yearBorn: Number,
+  'verified?': Boolean,
+});
 
-  const author = new Profile({
-    firstName: 'Trygve',
-    lastName: 'Reenskaug',
-    yearBorn: 1930,
-  });
+const author = new Profile({
+  firstName: 'Trygve',
+  lastName: 'Reenskaug',
+  yearBorn: 1930,
+});
 
-  console.log(`${author}`);
-  // [object Model]
+console.log(`${author}`);
+// [object Model]
 
-  console.log(author);
-  // {
-  //   firstName: 'Trygve',
-  //   lastName: 'Reenskaug',
-  //   yearBorn: 1930,
-  //   verified: null
-  // }
-};
-
-example1();
+console.log(JSON.stringify(author, null, '  '));
+// {
+//   firstName: 'Trygve',
+//   lastName: 'Reenskaug',
+//   yearBorn: 1930,
+//   verified: null
+// }
