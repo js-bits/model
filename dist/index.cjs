f3f60f82a0ca6feb023df5a39850fc8f7c602a14
ApoloGH <apolo-gh@users.noreply.github.com>
2023-02-15 15:23:23 -0500

    feat: export Collection

--- dist/index.cjs
+++ dist/index.cjs
@@ -479,7 +479,7 @@ class Model {
 
 class Schema extends Schema$1 {
   initType(propType) {
-  if (DataType.is(JSON, propType)) return new Model(propType);
+    if (DataType.is(JSON, propType)) return new Model(propType);
     // if (Model.isModel(propType) && !DataType.exists(propType)) {
     //   return propType;
     // }
@@ -505,5 +505,126 @@ Object.assign(Model, STATIC_PROPS);
 Object.assign(Model, ERRORS);
 Object.freeze(Model);
 
+/* eslint-disable max-classes-per-file */
+
+// pseudo-private properties emulation in order to avoid source code transpiling
+// TODO: replace with #privateField syntax when it gains wide support
+enumerate`
+  ContentModel
+  data
+  options
+`;
+
+const Options = new Model({
+  'min?': Number,
+  'max?': Number,
+});
+
+// const Model1 = new Model({
+//   numbers: [Number], // auto create new Collection(Number) // { min: undefined, max: undefined }
+//   // eslint-disable-next-line no-sparse-arrays
+//   numbers2: [Number, { min: 1, max: 10 }], // new Collection(Number, { min: 1, max: 10 })
+//   // eslint-disable-next-line no-sparse-arrays
+//   numbers5: [Number, , , ,], // new Collection(Number, { min: undefined, max: 4 })
+//   values: [Number, String, null], // multiple types
+// });
+
+class Collection extends Model {
+  static toString() {
+    return '[class Collection]';
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  get [Symbol.toStringTag]() {
+    return 'Collection';
+  }
+
+  constructor(ContentType, options = {}) {
+    super({
+      type: ContentType,
+    });
+    // if (Model.isModel(Type)) {
+    //   this.Model = Type;
+    // } else {
+    //   this.Model = new Model(Type);
+    // }
+    // this[ø.data] = new Map();
+    /* this[ø.options] = */ new Options(options);
+    console.log(this, options);
+  }
+
+  // add(item) {
+  //   if (item instanceof this.Model) {
+  //     // test
+  //   }
+  //   return this;
+  // }
+
+  // get(id) {
+  //   return this[ø.data].get(id);
+  // }
+
+  // delete(id) {
+  //   this[ø.data].delete(id);
+  //   return this;
+  // }
+}
+
+class CollectionSchema extends Schema {
+  initType(propType) {
+    console.log('CollectonSchema');
+    if (Array.isArray(propType)) {
+      const [contentType, ...rest] = propType;
+      let options;
+      if (propType.length > 1) {
+        [options] = rest;
+        if (options === undefined) {
+          if (rest.every(item => item === undefined)) {
+            options = {
+              max: propType.length,
+            };
+          }
+        }
+      }
+
+      return new Collection(contentType, options);
+    }
+    return super.initType(propType);
+  }
+}
+
+Schema.setGlobalSchema(CollectionSchema);
+
+new Collection(Number);
+
+const Field = new Model({
+  name: String,
+  type: String,
+  value: String,
+});
+
+new Model({
+  // eslint-disable-next-line no-sparse-arrays
+  fields: [Field],
+});
+
+new Model({
+  // eslint-disable-next-line no-sparse-arrays
+  fields: [Field, , , ,],
+});
+
+new Model({
+  // eslint-disable-next-line no-sparse-arrays
+  fields: [
+    Field,
+    {
+      min: 1,
+      max: 2,
+      // id: 'UUID',
+    },
+  ],
+});
+
+exports.Collection = Collection;
 exports.DataType = DataType;
 exports.Model = Model;
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-export { default as Model } from './src/model.js';
 export { default as DataType } from './src/data-type.js';
+export { default as Model } from './src/model.js';
+export { default as Collection } from './src/collection.js';
