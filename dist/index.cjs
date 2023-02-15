ec5bbe44156d7739efda786ed003d0104b95ed7b
ApoloGH <apolo-gh@users.noreply.github.com>
2023-02-15 12:00:28 -0500

    chore: upgrade dependencies and setup rollup build

---
+++ dist/index.cjs
@@ -0,0 +1,520 @@
+'use strict';
+
+var enumerate = require('@js-bits/enumerate');
+
+/* eslint-disable max-classes-per-file */
+
+const DATA_TYPES = new WeakMap();
+
+const ERRORS$2 = enumerate(String)`
+InvalidDataTypeError
+UnknownDataTypeError
+`;
+
+class DataType {
+  static toString() {
+    return '[class DataType]';
+  }
+
+  constructor(config) {
+    if (arguments.length) {
+      let typeDef;
+
+      class NewDataType extends DataType {
+        constructor() {
+          const error = new Error('Data type instantiation is not allowed');
+          error.name = ERRORS$2.InvalidDataTypeError;
+          throw error;
+        }
+
+        // name // for GraphQL conversion
+        // compare // for sorting
+
+        static fromJSON(inputValue) {
+          this.validate(inputValue, true);
+          if (typeDef.fromJSON) return typeDef.fromJSON(inputValue);
+          return inputValue;
+        }
+
+        static toJSON(value) {
+          let outputValue = value;
+          if (typeDef.toJSON) outputValue = typeDef.toJSON(value);
+          this.validate(outputValue, true);
+          return outputValue;
+        }
+
+        static validate(value, hardCheck) {
+          const result = DataType.validate(NewDataType, value);
+          if (result && hardCheck) {
+            const error = new Error('Data type is invalid');
+            error.name = ERRORS$2.InvalidDataTypeError;
+            throw error;
+          }
+          return result;
+        }
+      }
+
+      typeDef = DataType.add(NewDataType, config);
+      // eslint-disable-next-line no-constructor-return
+      return NewDataType;
+    } // else prototype is being created
+  }
+
+  static add(type, config) {
+    let validator;
+    if (typeof config === 'function') {
+      validator = config;
+    } else if (config && typeof config === 'object' && typeof config.validate === 'function') {
+      validator = config.validate;
+    }
+    if (!validator) {
+      const error = new Error('Data type is invalid');
+      error.name = ERRORS$2.InvalidDataTypeError;
+      throw error;
+    }
+
+    const typeDef = {
+      validate: validator,
+    };
+
+    if (typeof config === 'object') {
+      if (Object.prototype.hasOwnProperty.call(config, 'extends')) {
+        if (!DataType.exists(config.extends)) {
+          const error = new Error('Base data type is invalid');
+          error.name = ERRORS$2.InvalidDataTypeError;
+          throw error;
+        }
+        typeDef.extends = config.extends;
+      }
+      if (
+        Object.prototype.hasOwnProperty.call(config, 'fromJSON') ||
+        Object.prototype.hasOwnProperty.call(config, 'toJSON')
+      ) {
+        if (typeof config.fromJSON !== 'function' || typeof config.toJSON !== 'function') {
+          const error = new Error('Both "fromJSON" and "toJSON" must defined as functions');
+          error.name = ERRORS$2.InvalidDataTypeError;
+          throw error;
+        }
+        typeDef.fromJSON = config.fromJSON;
+        typeDef.toJSON = config.toJSON;
+      }
+    }
+
+    Object.freeze(typeDef);
+
+    DATA_TYPES.set(type, typeDef);
+
+    return typeDef;
+  }
+
+  static exists(type) {
+    return DATA_TYPES.has(type);
+  }
+
+  static get(type) {
+    const typeDef = DATA_TYPES.get(type);
+    if (!typeDef) {
+      const error = new Error('Unknown data type');
+      error.name = ERRORS$2.UnknownDataTypeError;
+      throw error;
+    }
+    return typeDef;
+  }
+
+  /**
+   * Validates a value against a given type
+   * @param {Object} type
+   * @param {*} value
+   * @returns {Array}
+   */
+  static validate(type, value) {
+    const typeDef = DataType.get(type);
+    let error;
+    if (typeDef.extends) {
+      error = DataType.validate(typeDef.extends, value);
+    }
+
+    // if no error messages from a base validator
+    if (!error) {
+      const validator = typeDef.validate;
+      error = validator(value);
+    }
+
+    return error;
+  }
+
+  static is(type, value) {
+    return !DataType.validate(type, value);
+  }
+}
+
+DataType.add(String, value => (typeof value === 'string' ? undefined : 'must be a string'));
+DataType.add(Number, value => (typeof value === 'number' ? undefined : 'must be a number'));
+DataType.add(Boolean, value => (typeof value === 'boolean' ? undefined : 'must be a boolean'));
+DataType.add(Date, value => (value instanceof Date ? undefined : 'must be a date'));
+DataType.add(DataType, value =>
+  typeof value === 'function' && Object.getPrototypeOf(value) === DataType ? undefined : 'must be a data type'
+);
+DataType.add(JSON, value =>
+  !enumerate.isEnum(value) && value !== JSON && value instanceof Object && value.constructor === Object
+    ? undefined
+    : 'must be a plain object'
+);
+
+Object.assign(DataType, ERRORS$2);
+
+const ERRORS$1 = enumerate(String)`
+InvalidModelSchemaError
+InvalidDataError
+`;
+
+const REQUIRED_FIELD_SPECIFIER = '!';
+const OPTIONAL_FIELD_SPECIFIER = '?';
+const FIELD_NAME_REGEXP = /^(.+)([?!])$/;
+
+// pseudo-private properties emulation in order to avoid source code transpiling
+// TODO: replace with #privateField syntax when it gains wide support
+const ø = enumerate`
+required
+requiredFlag
+`;
+
+/**
+ * Supports only primitive data types (defined with DataType class) by default
+ */
+let Schema$1 = class Schema {
+  constructor(config) {
+    if (!DataType.is(JSON, config)) {
+      const error = new Error('Model schema is invalid');
+      error.name = ERRORS$1.InvalidModelSchemaError;
+      throw error;
+    }
+
+    if (Object.keys(config).length === 0) {
+      const error = new Error('Model schema is empty');
+      error.name = ERRORS$1.InvalidModelSchemaError;
+      throw error;
+    }
+
+    this[ø.required] = {};
+    for (const [key, type] of Object.entries(config)) {
+      this.processEntry(key, type);
+    }
+
+    Object.freeze(this);
+  }
+
+  processEntry(key, type) {
+    const propName = this.initKey(key);
+    const propType = this.initType(type);
+
+    if (!propType) {
+      const error = new Error(`Model schema is invalid: data type of "${propName}" property is invalid`);
+      error.name = ERRORS$1.InvalidModelSchemaError;
+      throw error;
+    }
+    this[propName] = propType;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  initType(propType) {
+    if (DataType.exists(propType)) {
+      return propType;
+    }
+    if (enumerate.isEnum(propType) && !DataType.exists(propType)) {
+      DataType.add(propType, value => {
+        const allowedValues = Object.values(propType);
+        const list = allowedValues.map(item => String(item)).join(',');
+        return allowedValues.includes(value) ? undefined : `must be one of allowed values [${list}]`;
+      });
+      return propType;
+    }
+  }
+
+  initKey(key) {
+    let specifier;
+    let propName = key;
+    const globalFlag = this[ø.requiredFlag];
+    const match = key.match(FIELD_NAME_REGEXP);
+    if (match) {
+      [, propName, specifier] = match;
+      if (
+        globalFlag !== undefined &&
+        specifier !== (globalFlag ? REQUIRED_FIELD_SPECIFIER : OPTIONAL_FIELD_SPECIFIER)
+      ) {
+        const error = new Error(
+          `Model schema is invalid. Must contain either ${OPTIONAL_FIELD_SPECIFIER} or ${REQUIRED_FIELD_SPECIFIER} specifiers`
+        );
+        error.name = Schema.InvalidModelSchemaError;
+        throw error;
+      }
+      this[ø.requiredFlag] = specifier === REQUIRED_FIELD_SPECIFIER;
+    }
+    this[ø.required][propName] = !specifier;
+
+    return propName;
+  }
+
+  getKeys(data) {
+    console.log('getKeys', data);
+    if (!DataType.is(JSON, data)) {
+      const error = new Error('Model data must be a plain object');
+      error.name = ERRORS$1.InvalidDataError;
+      throw error;
+    }
+    return new Set([...Object.keys(this), ...Object.keys(data)]);
+  }
+
+  isRequired(name) {
+    return this[ø.required][name] === !this[ø.requiredFlag];
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  validate(propType, propValue) {
+    return DataType.validate(propType, propValue);
+  }
+
+  transformType(name) {
+    return this[name];
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  transformValue(propType, propValue) {
+    console.log('transformValue3', propType, propValue);
+    if (DataType.is(DataType, propType)) return propType.fromJSON(propValue);
+    return propValue;
+  }
+
+  static setGlobalSchema(GlobalSchema) {
+    globalSchema = GlobalSchema;
+  }
+
+  static getGlobalSchema() {
+    return globalSchema;
+  }
+};
+
+let globalSchema = Schema$1;
+
+Object.assign(Schema$1, ERRORS$1);
+Object.freeze(Schema$1);
+
+/**
+ * This is just a part of Model extracted for convenience
+ * @param {Class} Model
+ * @param {Object} data
+ * @param {Schema} schema
+ * @returns {Object}
+ */
+function assemble(Model, data, schema) {
+  const shouldInstantiate = !!this;
+  const validationResult = {};
+  console.log('assemble', data);
+  for (const propName of schema.getKeys(data)) {
+    console.log('propName', propName);
+    const propType = schema.transformType(propName);
+    console.log('propType', propType);
+    if (propType) {
+      const propValue = data[propName];
+      const isDefined = !(propValue === undefined || propValue === null);
+      if (isDefined) {
+        const errors = schema.validate(propType, propValue);
+        if (errors) validationResult[propName] = errors;
+      } else if (schema.isRequired(propName)) {
+        validationResult[propName] = 'required property is not defined';
+      }
+
+      if (shouldInstantiate && !validationResult[propName])
+        // intentionally set to null for both cases (undefined and null)
+        this[propName] = isDefined ? schema.transformValue(propType, propValue) : null;
+    } else {
+      validationResult[propName] = 'property is not defined in schema';
+    }
+  }
+
+  const hasErrors = Object.keys(validationResult).length;
+  if (shouldInstantiate) {
+    if (hasErrors) {
+      const error = new Error('Invalid data');
+      error.name = Model.InvalidDataError;
+      error.cause = validationResult; // TODO: replace with native https://v8.dev/features/error-cause;
+      throw error;
+    }
+    return this;
+  }
+
+  return hasErrors ? validationResult : undefined;
+}
+
+/**
+ * This is just a part of Model extracted for convenience
+ * @param {Class} Model
+ * @param {Object} schema
+ * @returns {Class}
+ */
+const create = (Model, schema) => {
+  // NOTE: encapsulated class definition makes it impossible to manipulate data schema from outside of the model
+  class NewModel extends Model {
+    constructor(data) {
+      super();
+      this.assemble(data);
+
+      const proxy = new Proxy(this, {
+        get(...args) {
+          const [target, prop] = args;
+          const allowedProps = [Symbol.toPrimitive, Symbol.toStringTag, 'toJSON', 'toString', 'constructor'];
+          if (!Object.prototype.hasOwnProperty.call(target, prop) && !allowedProps.includes(prop)) {
+            throw new Error(`Property "${String(prop)}" of a Model instance is not accessible`);
+          }
+          return Reflect.get(...args);
+        },
+        set(target, prop) {
+          throw new Error(`Property assignment is not supported for "${String(prop)}"`);
+        },
+      });
+
+      // eslint-disable-next-line no-constructor-return
+      return proxy;
+    }
+
+    assemble(data) {
+      assemble.call(this, NewModel, data, schema);
+    }
+
+    /**
+     * @param {*} data
+     * @returns {Object} - an object representing validation errors
+     */
+    static validate(data) {
+      return assemble(NewModel, data, schema);
+    }
+
+    // static toGraphQL() {}
+  }
+
+  Object.freeze(NewModel);
+
+  return NewModel;
+};
+
+/* eslint-disable max-classes-per-file */
+
+const MODELS = new WeakSet();
+
+const STATIC_PROPS = enumerate`
+  SAME
+`;
+
+const ERRORS = enumerate(String)`
+InvalidDataError
+`;
+
+class Model /* extends DataType */ {
+  static toString() {
+    return '[class Model]';
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  get [Symbol.toStringTag]() {
+    return 'Model';
+  }
+
+  constructor(config) {
+    if (arguments.length) {
+      let NewModel;
+
+      // eslint-disable-next-line no-use-before-define
+      class Schema extends Schema$1.getGlobalSchema() {
+        initType(propType) {
+          if (propType === Model.SAME) return Model.SAME;
+          return super.initType(propType);
+        }
+
+        transformType(propName) {
+          const propType = super.transformType(propName);
+          if (propType === Model.SAME) return NewModel;
+          return propType;
+        }
+      }
+
+      NewModel = create(this.constructor, new Schema(config));
+
+      DataType.add(NewModel, {
+        extends: Model,
+        // fromJSON(value) {
+        //   return new NewModel(value);
+        // },
+        // toJSON(value) {
+        //   return value.toJSON;
+        // },
+        validate(value) {
+          return value instanceof NewModel ? undefined : 'invalid model type';
+        },
+      });
+
+      // const DataTypeRef = super({
+      //   extends: Model,
+      //   validate(value) {
+      //     return value instanceof NewModel ? undefined : 'must be a specified model';
+      //   },
+      //   // fromJSON: inputValue => {
+      //   //   if (Model.isModel(inputValue) ? DataType.is(JSON, propValue)
+      //   //   return new NewModel(inputValue)
+      //   // },
+      // });
+
+      // NewModel.fromJSON = DataTypeRef.fromJSON;
+
+      // console.log('NewModel.fromJSON', NewModel.fromJSON, DataTypeRef.fromJSON);
+
+      // DataType.add(NewModel, DataType.get(DataTypeRef));
+
+      // Move this to StorageModel (extends Model)
+      // MODELS.set(NewModel.ID, NewModel);
+      MODELS.add(NewModel);
+      // NewModel.ID = Symbol('Model ID'); // do I really need it?
+      // NewModel.validateOnInit = true; // by default
+
+      // eslint-disable-next-line no-constructor-return
+      return NewModel;
+    } // else prototype is being created
+    // super();
+  }
+
+  // fromJSON() {}
+  // sync() {}
+  static isModel(type) {
+    return typeof type === 'function' && MODELS.has(type);
+  }
+}
+
+class Schema extends Schema$1 {
+  initType(propType) {
+    if (DataType.is(JSON, propType)) return new Model(propType);
+    // if (Model.isModel(propType) && !DataType.exists(propType)) {
+    //   return propType;
+    // }
+    return super.initType(propType);
+  }
+
+  validate(propType, propValue) {
+    if (Model.isModel(propType) && DataType.is(JSON, propValue)) return propType.validate(propValue);
+    return super.validate(propType, propValue);
+  }
+
+  transformValue(PropType, propValue) {
+    console.log('transformValue2', PropType, propValue);
+    if (Model.isModel(PropType) && DataType.is(JSON, propValue)) return new PropType(propValue);
+    return super.transformValue(PropType, propValue);
+  }
+}
+
+Schema$1.setGlobalSchema(Schema);
+
+DataType.add(Model, value => (value instanceof Model ? undefined : 'must be a model'));
+
+Object.assign(Model, STATIC_PROPS);
+Object.assign(Model, ERRORS);
+Object.freeze(Model);
+
+exports.Model = Model;
